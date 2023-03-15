7352b827a2742329990e938bb6d2d4251e3fb5a5
Rohit Parihar <rohitparihar07125@gmail.com>
2023-03-15 21:12:07 +0530

    Update hsnApis.ts

--- client/src/api/hsnApis.ts
+++ client/src/api/hsnApis.ts
@@ -1,25 +1,30 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import axios from 'axios'
+import axios from "axios";
 import { mockHsnResponse, updatedHsnResponse } from "../mockDataHsn";
 
 export const getHsnList = createAsyncThunk("user/getHsnList", async () => {
-    // need replace with actual Api
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts`
-    );
-    return mockHsnResponse;
-  });
-  export const updateHsnList = createAsyncThunk("user/updateHsnList", async () => {
+  // need replace with actual Api
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts`
+  );
+  return mockHsnResponse;
+});
+
+export const updateHsnList = createAsyncThunk(
+  "user/updateHsnList",
+  async () => {
     // need replace with actual Api
     const response = await axios.get(
       `https://jsonplaceholder.typicode.com/posts`
     );
     return updatedHsnResponse;
-  });
-  export const deleteHsn = createAsyncThunk("user/deleteHsnList", async () => {
-    // need replace with actual Api
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts`
-    );
-    return response;
-  });
+  }
+);
+
+export const deleteHsn = createAsyncThunk("user/deleteHsnList", async () => {
+  // need replace with actual Api
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts`
+  );
+  return response;
+});
